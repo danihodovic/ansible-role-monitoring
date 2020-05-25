f3b9152521d31aa2bd90ddd1495fe458ecb71ab1
Dani Hodovic <dani.hodovic@gmail.com>
2020-05-25 20:25:56 +0300

    Initial commit

---
+++ tests/test_all.py
@@ -0,0 +1,35 @@
+# pylint: disable=redefined-outer-name,invalid-name
+import time
+import testinfra
+import pytest
+import requests
+
+@pytest.fixture
+def docker_host(host):
+    def fn(container_name):
+        container = host.docker(container_name)
+        assert container.is_running
+        return testinfra.get_host(f"docker://{container.id}")
+
+    return fn
+
+
+@pytest.mark.parametrize("container", ["prometheus", "alertmanager", "grafana"])
+def test_containers_running(container, host):
+    assert host.docker(container).is_running
+
+
+def test_containers_can_ping_prometheus(docker_host):
+    cmd = 'nc -vz prometheus 9090'
+    docker_host("grafana").run_test(cmd)
+    docker_host("alertmanager").run_test(cmd)
+
+
+@pytest.mark.skip(reason="needs session auth")
+def test_grafana_datasource_prometheus(_host):
+    # Emulate the request Grafana makes when trying to reach the Prometheus
+    # source
+    requests.get(
+        "http://localhost:9099/api/datasources/proxy/1/api/v1/query",
+        params={"query": "1+1", "time": int(time.time())},
+    )
